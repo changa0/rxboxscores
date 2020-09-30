738cf84632f9aa2c4ed94f7e301fb5b762af8d0b
changa0 <changa.ghub@gmail.com>
2020-09-29 18:29:10 -0700

    Updates

---
+++ precache-manifest.913afefff2509ab5fa5775ee6c33ccd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41dcacd732f06aaed772047bf037fd02",
+    "url": "/rxboxscores/index.html"
+  },
+  {
+    "revision": "e9fee8987e030ea31349",
+    "url": "/rxboxscores/static/css/main.d59c5262.chunk.css"
+  },
+  {
+    "revision": "7a6714e80cb2d2d901cc",
+    "url": "/rxboxscores/static/js/2.96c88239.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rxboxscores/static/js/2.96c88239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9fee8987e030ea31349",
+    "url": "/rxboxscores/static/js/main.306213dd.chunk.js"
+  },
+  {
+    "revision": "26504f4431fd2651bd78",
+    "url": "/rxboxscores/static/js/runtime-main.28c9a1fe.js"
+  }
+]);
